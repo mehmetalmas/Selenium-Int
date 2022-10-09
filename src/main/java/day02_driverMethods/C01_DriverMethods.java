3a135fbf295f3f7bf38d905dcb8f28ae9adde3a0
Mehmet Almas <m.almas63@gmail.com>
2022-10-09 12:07:38 +0200

    güncelleme091022

--- src/main/java/day02_driverMethods/C01_DriverMethods.java
+++ src/main/java/day02_driverMethods/C01_DriverMethods.java
@@ -4,15 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
     public class C01_DriverMethods {
-        public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
+        public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
+            System.setProperty("webdriver.chrome.driver","src/resources/chromedriver");
 
-        driver.get("https://www.amazon.com");
+            WebDriver driver= new ChromeDriver();
+            // driver objesi olusturunca Selenium webDriver
+            // bilgisayarimizdaki chrome browser'dan bir bos sayfa actirir
+            // eger firefox veya safari gibi baska bir browser kullanmak istersek
+            // Selenium sitesinden o browser'in WebDriver'ini indirip projemize eklememiz gerekir
 
-        driver.close();
+            driver.get("https://www.amazon.com");
+            // eger kodlarimizi belirli bir sure bekletmek istersek
 
+            Thread.sleep(5000);// yazilan milisaniye kadar kodlari bekletir
+
+            System.out.println("sayfa url'i : "+driver.getCurrentUrl());
+
+            System.out.println("Saning title'i : "+driver.getTitle());
+
+            driver.close();
 
 
 
---
+++ src/main/java/day02_driverMethods/C03_DriverGetMethodlari.java
@@ -0,0 +1,29 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C03_DriverGetMethodlari {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver");
+        WebDriver driver= new ChromeDriver();
+
+        System.out.println(driver.getWindowHandle());
+        // bize acilan browser'in handle degerini dondurur
+        // CDwindow-8C07925B8CBA4C8EF3039F660C30DDA1
+        // CDwindow-8C82D85FA29AED859A8A035A3D8C2337
+
+
+        System.out.println(driver.getWindowHandles());
+        /*
+        Eger testimiz sirasinda birden fazla window acilirsa
+        acilan tum window'lari window handle degerlerini bir Set olarak dondurur
+        window'lar arasi gecisi bu handle degerleri ile yapariz
+         */
+
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getPageSource());
+        // websayfasinin tum kaynak kodlarini döndürür
+        driver.close();
+    }
+}
