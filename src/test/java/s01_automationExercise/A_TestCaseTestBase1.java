6d7694a12c0f9f2b679e548dd4ab8e83541889a8
Mehmet Almas <m.almas63@gmail.com>
2022-10-19 16:05:11 +0200

    güncelleme191022

--- src/test/java/s01_automationExercise/A_TestCaseTestBase1.java
+++ src/test/java/s01_automationExercise/A_TestCaseTestBase1.java
@@ -6,12 +6,26 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) // @Test notasyonlarini yazildigi siraya göre calistirir
 
+
+/*
+ * Java dilinde Annotation(Notasyonlar), bir veri hakkında bilgi barındıran veriyi sağlayan basit bir yapıdır.
+ * Bu sağladığı bilgiye de “metadata” denir.
+ * Notasyonlar( Annotation) genellikle Java’da konfigürasyon amacıyla kullanılır. Ya da bir bileşene ek özellikle katar.
+ * Bu bileşenler sınıf, metod, değişkenler, paket ya da parametreler olabilir. Bunların hepsinde notasyonları kullanabiliriz.
+ */
 
 public class A_TestCaseTestBase1 {
 
-    protected static WebDriver driver;
+    protected static WebDriver driver;  // Static olarak WebDriver i olusturduk
+
+
+    // @Before notasyonu = eger test adimlarimiz birbirine bagli(irtibatli) ise testlerimizi bir tek @Test notasyonu altinda calistirir
+    // Testlerin birbirine bagli olmasinin anlami; ayni WebDriver i kullanmalari anlamina gelmektedir
+    // @Before olusturulan WebDriver bir tek @Test notayonu icin kullanilabilir vaya calistirilabilir
+    // @Before kullanarak birbirine bagli test adimlarini farkli @Test notasyonlari altinda yapmak hataya sebep olur (Farkli WebDriverler'a ihtiyac olacagindan )
+
     @Before
     public void setup(){
         WebDriverManager.chromedriver().setup();
--- src/test/java/s01_automationExercise/A_TestCaseTestBase2.java
+++ src/test/java/s01_automationExercise/A_TestCaseTestBase2.java
@@ -6,13 +6,29 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) // @Test notasyonlarini yazildigi siraya göre calistirir
+
+
+
+/*
+ * Java dilinde Annotation(Notasyonlar), bir veri hakkında bilgi barındıran veriyi sağlayan basit bir yapıdır.
+ * Bu sağladığı bilgiye de “metadata” denir.
+ * Notasyonlar( Annotation) genellikle Java’da konfigürasyon amacıyla kullanılır. Ya da bir bileşene ek özellikle katar.
+ * Bu bileşenler sınıf, metod, değişkenler, paket ya da parametreler olabilir. Bunların hepsinde notasyonları kullanabiliriz.
+ */
 
 
 
 public class A_TestCaseTestBase2 {
 
-    protected static WebDriver driver;
+    protected static WebDriver driver; // Static olarak WebDriver i olusturduk
+
+
+    // @BeforeClass notasyonu = eger test adimlarimiz birbirine bagli(irtibatli) ise testlerimizi bir den fazla @Test notasyonu altinda calistirabilir
+    // Testlerin birbirine bagli olmasinin anlami; ayni WebDriver i kullanmalari anlamina gelmektedir
+    // @BeforeClass olusturulan WebDriver birden fazla @Test notayonu icin kullanilabilir vaya calistirilabilir
+    // @Before kullanarak birbirine bagli test adimlarini farkli @Test notasyonlari altinda yapilabilir (bir WebDriverler tüm @Test icin ayni sekilde üretileceginden )
+
     @BeforeClass
     public static void setup(){
         WebDriverManager.chromedriver().setup();
@@ -21,6 +37,8 @@ public class A_TestCaseTestBase2 {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         System.out.println(driver);
     }
+
+
     @AfterClass
     public static void tearDown(){
         //driver.quit();
